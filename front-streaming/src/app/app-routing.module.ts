2c3ad7665b7aeb5e3b8467b2deaffb90db615a0c
Fuji vanilli <ijufillivan@gmail.com>
2024-01-09 15:12:28 +0300

    add pages home movie details

--- front-streaming/src/app/app-routing.module.ts
+++ front-streaming/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'movie/:id', component: MovieDetailsComponent},
+  /*{ path: 'search', component: SearchComponent, canActivate: [AuthGuard], data: { roles:[]} },
+ 
+  { path: 'login', component: LoginComponent},
+  { path: 'singup', component: SingupComponent},
+  { path: 'cardMovie/:category', component: CardMovieComponent},
+  { path: 'dashboard', component: DashboardComponent}*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-streaming/src/app/app.module.ts
+++ front-streaming/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
