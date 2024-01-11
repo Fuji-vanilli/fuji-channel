7ce27d2ce42607652e2f53febce644391f923ded
Fuji vanilli <ijufillivan@gmail.com>
2024-01-11 13:18:48 +0300

    deleting front

--- front-streaming/src/app/models/movie.ts
+++
@@ -1,4 +0,0 @@
-export interface  Movie {
-    category: string,
-    url: string
-}
--- front-streaming/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment= {
-    backendMovieHost: 'https://api.themoviedb.org/3',
-    backendSpring: 'http://localhost:8030/api/movie'
-}
